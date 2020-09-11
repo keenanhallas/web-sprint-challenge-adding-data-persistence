ffdb789b164be4070c95cc964d0d3a7c2c6c0d42
Keenan <keenanhallas@gmail.com>
2020-09-11 15:29:54 -0500

    added mvp routes - accidentally did stretch task routes before

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const helmet = require("helmet");
+const db = require("../data/connection");
 
 //import routes here?
 const projectsRouter = require("../projects/projects-router");
@@ -11,6 +12,26 @@ server.use(express.json());
 
 server.get("/", (req, res) => {
     res.status(200).send("<h1>Data Persistence Sprint Challenge!</h1>");
+});
+
+server.get("/resources", (req, res) => {
+    db("resources")
+        .then(resources => {
+            res.status(200).json({ resources: resources });
+        })
+        .catch(err => {
+            res.status(500).json({ error: err.message });
+        })
+});
+
+server.get("/tasks", (req, res) => {
+    db("tasks")
+        .then(tasks => {
+            res.status(200).json({ tasks: tasks });
+        })
+        .catch(err => {
+            res.status(500).json({ error: err.message });
+        })
 });
 
 server.use("/projects", projectsRouter);
